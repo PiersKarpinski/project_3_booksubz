62b18726104c3a55a4ae37e76bef4f73676d1267
Piers Karpinski <piers.karpinski@gmail.com>
2014-09-08 12:56:40 +0100

    initial commit: basic app with gems. About to scaffold and devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BooksubzApp::Application.config.secret_token = 'd6c7ceaad4bb36bfe29089bc1e1dcc41cf2bd447ff1c9a11b200c4c5559e6e91248d6d5cd5a04b6ce1d931df4a4aa11ad23fcd22e9db8a6d105066a9a8760256'
