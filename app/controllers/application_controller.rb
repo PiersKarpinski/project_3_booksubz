dc73e3a950861b6f99153a1d125951c1cf63b1ce
Piers Karpinski <piers.karpinski@gmail.com>
2014-09-10 14:36:56 +0100

    category index page taking shape as is category drop down

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,19 @@ class ApplicationController < ActionController::Base
   #PK this before filter and method sets up the ransack search so that it can run successfully in the nav bar.
   before_filter :set_ransack_form_variables
 
+  
+  #PK ensures that categories are loaded into the drop down in the nav bar
+  before_filter :set_categories
+
   #PK This method effectively replaces the index method in albums controller.
   def set_ransack_form_variables
     @q = Book.search(params[:q])
     @books = @q.result(distinct: true).order(:created_at).page(params[:page])
   end
+
+  def set_categories
+    @categories = Category.all
+  end
+
+
 end
