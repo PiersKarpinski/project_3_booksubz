72de442cce4d6487e1c6f5f3353533c2dd2ce00e
Piers Karpinski <piers.karpinski@gmail.com>
2014-09-14 14:00:17 +0100

    letter opener added as well as devise confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -73,10 +73,15 @@ ActiveRecord::Schema.define(:version => 20140909104419) do
     t.text     "image"
     t.text     "about"
     t.date     "dob"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
