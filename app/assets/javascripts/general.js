e04babd7dc1cc449ef3a39866d9338bc5b4720e5
Piers Karpinski <piers.karpinski@gmail.com>
2014-09-15 12:25:26 +0100

    code in but not working

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -2,7 +2,7 @@
 
   var app = angular.module('booksubzApp', []);
 
- 
+  //basket 
 
   app.controller('BasketController', ["$scope", function($scope){
       $scope.basket = 0;
@@ -19,8 +19,53 @@
 
     }]);  
 
+  //rating
 
+  app.controller('RatingDemoCtrl', ["$scope", function($scope){
 
+    $scope.rate = 7;
+    $scope.max = 10;
+    $scope.isReadonly = false;
+
+    $scope.hoveringOver = function(value) {
+      $scope.overStar = value;
+      $scope.percent = 100 * (value / $scope.max);
+    };
+
+    $scope.ratingStates = [
+      {stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+      {stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+      {stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+      {stateOn: 'glyphicon-heart'},
+      {stateOff: 'glyphicon-off'}
+    ];
+  
+  }])
+
+  app.controller('ModalDemoCtrl', ["$scope", "$modal", "$log", function($scope, $modal, $log){
+
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function (size) {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'myModalContent.html',
+        controller: ModalInstanceCtrl,
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  }])
 
 })();
 
