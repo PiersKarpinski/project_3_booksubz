589f94a8278b430941e91d26b0264c5d94870802
Piers Karpinski <piers.karpinski@gmail.com>
2014-09-16 11:03:07 +0100

    change redirect after creating a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -43,10 +43,11 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(params[:comment])
+    @book = Book.all
     @comment.user_id = current_user.id
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to books_path, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
