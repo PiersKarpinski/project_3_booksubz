6ed245b6867d0ad3eda7e6806e8af31277d5e927
Piers Karpinski <piers.karpinski@gmail.com>
2014-09-16 10:20:04 +0100

    changed mandrill url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ BooksubzApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'booksubz.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'booksubz-app.herokuapp.com' }
 
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
